6f24102a724ad2927716493cf8276367bd00c442
leonardonadi@gmail.com <Mutley@1#2Dist@1#6>
2022-03-13 19:22:58 -0300

    9. Digitando em campos e clicando em elementos - exercício

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,11 +1,21 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+describe('Central de Atendimento ao Cliente TAT', function () {
 
+    beforeEach(() => {
         cy.visit('src/index.html')
+    });
+
+    it('verifica o título da aplicação', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  
     })
-  })
-  
+
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('#firstName').type('Leandro')
+        cy.get('#lastName').type('Pereira')
+        cy.get('#email').type('teste@teste.com') 
+        cy.get('#open-text-area').type('8. Digitando em campos e clicando em elementos')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    });
+})
