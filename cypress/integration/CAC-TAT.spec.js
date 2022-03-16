64e0ea393461b2092a149d6abff4bf97c665c243
leonardonadi@gmail.com <Mutley@1#2Dist@1#6>
2022-03-16 09:27:10 -0300

    11. Digitando em campos e clicando em elementos - exercício extra 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,12 +10,21 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', () => {
+    it('preenche os campos obrigatórios e envia o formulário', () => {
         cy.get('#firstName').type('Leandro')
         cy.get('#lastName').type('Pereira')
         cy.get('#email').type('teste@teste.com') 
         cy.get('#open-text-area').type('8. Digitando em campos e clicando em elementos',{ delay:0 })
         cy.get('button[type="submit"]').click()
         cy.get('.success').should('be.visible')
+    });
+
+    it.only('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+        cy.get('#firstName').type('Leandro')
+        cy.get('#lastName').type('Pereira')
+        cy.get('#email').type('testeteste.com') 
+        cy.get('#open-text-area').type('8. Digitando em campos e clicando em elementos')
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
     });
 })
