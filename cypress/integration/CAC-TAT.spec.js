17df6315e867caf24d247fb87254cc8900360998
leonardonadi@gmail.com <Mutley@1#2Dist@1#6>
2022-03-26 19:18:38 -0300

    35. Lidando com links que abrem em outra aba - exercício extra 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -164,11 +164,13 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#privacy a').should('have.attr','target','_blank')
     });
 
-    it.only('acessa a página da política de privacidade removendo o target e então clicanco no link', () => {
+    it('acessa a página da política de privacidade removendo o target e então clicanco no link', () => {
         cy.get('#privacy a')
             .invoke('removeAttr','target')
             .click()
 
         cy.contains('Talking About Testing').should('be.visible')
     });
+
+    
 })
