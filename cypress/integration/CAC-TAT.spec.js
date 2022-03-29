1925df4c3eca4f3518ddb1d6c8fbef88f13407b2
leonardonadi@gmail.com <Mutley@1#2Dist@1#6>
2022-03-29 08:26:17 -0300

    54. Avançando no uso do Cypress - cy.request() - exercício extra 4

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -213,4 +213,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('have.value', longText)
     });
 
+    it('faz uma requisição HTTP', () => {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response){
+                const {status, statusText, body} = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+            })
+    });
+
 })
